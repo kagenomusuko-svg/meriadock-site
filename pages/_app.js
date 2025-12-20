62894181e7e470ae5e577c2e053a4d4368aeab95
kagenomusuko-svg <kage.no.musuko@gmail.com>
2025-12-20 16:08:51 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '../styles/globals.css';
+import Head from 'next/head'; // Importamos el componente Head
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Head global para toda la aplicación */}
+      <Head>
+        <title>Meriadock</title> {/* Aquí defines el nombre que aparece en la pestaña */}
+        <link rel="icon" href="/favicon.ico" /> {/* Enlace al favicon */}
+      </Head>
+      {/* Renderizamos el resto de la página */}
+      <Component {...pageProps} />
+    </>
+  );
 }
